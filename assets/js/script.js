329b159d4e1a37a898ac56e2e9c1f1d446695729
Thamimul Ansari <thamimul2004@gmail.com>
2026-08-06 12:51:49 +0530

    Major updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -322,11 +322,11 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["full-stack development", "backend development", "frontend development", "web development", "AI-powered solutions", "database management"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
-    backDelay: 500,
+    backDelay: 600,
 });
 // <!-- typed js effect ends -->
 
